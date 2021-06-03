4fbffa43d7c1e5e02dada88371e4167b3b91eeef
r.wiebenga@umcutrecht.nl <r.wiebenga@umcutrecht.nl>
2021-06-03 18:06:38 +0200

    Static version of wardrobe

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace WardrobeCursus
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //var myWardrobe = new Wardrobe()
+            //{
+            //    Material = "wood",
+            //    AmountDoors = 1
+            //};
+            //myWardrobe.OpenDoor();
+            //myWardrobe.OpenDoor();
+            //myWardrobe.OpenDoor();
+
+            StaticWardrobe.Instance.Material = "wood";
+            StaticWardrobe.Instance.AmountDoors = 1;
+            StaticWardrobe.Instance.OpenDoor();
+            StaticWardrobe.Instance.OpenDoor();
+            StaticWardrobe.Instance.CloseDoor();
+            StaticWardrobe.Instance.CloseDoor();
+
+        }
+    }
+}
